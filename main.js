81881552450a21107a175a72d743bfa8145a0465
Ben Holmes <7benholmes@gmail.com>
2023-11-14 22:14:03 -0800

    start masking out runs

--- main.js
+++ main.js
@@ -1,8 +1,457 @@
 runs = [
 	{
 		name: "Knob Hill",
-
-	}
+		boxes: [
+			{
+				left: 494,
+				top: 3439,
+				width: 145,
+				height: 57,
+				rotationDeg: 90,
+			}
+		]
+	},
+	{
+		name: "Lake View",
+		boxes: [
+			{
+				left: 655,
+				top: 3579,
+				width: 155,
+				height: 57,
+				rotationDeg: 17,
+			},
+			{
+				left: 712,
+				top: 2812,
+				width: 155,
+				height: 57,
+				rotationDeg: 45,
+			}
+		]
+	},
+	{
+		name: "Valley View",
+		boxes: [
+			{
+				left: 689,
+				top: 3022,
+				width: 180,
+				height: 57,
+				rotationDeg: -35,
+			}
+		]
+	},
+	{
+		name: "Montezuma's",
+		boxes: [
+			{
+				left: 759,
+				top: 2907,
+				width: 210,
+				height: 57,
+				rotationDeg: -45,
+			}
+		]
+	},
+	{
+		name: "Strainer",
+		boxes: [
+			{
+				left: 958,
+				top: 2691,
+				width: 210,
+				height: 57,
+				rotationDeg: -55,
+			}
+		]
+	},
+	{
+		name: "Poulsen's Gully",
+		boxes: [
+			{
+				left: 1570,
+				top: 3100,
+				width: 300,
+				height: 57,
+				rotationDeg: -85,
+			}
+		]
+	},
+	{
+		name: "Champs Elysees",
+		boxes: [
+			{
+				left: 1316,
+				top: 2640,
+				width: 300,
+				height: 57,
+				rotationDeg: -5,
+			},
+			{
+				left: 2152,
+				top: 2828,
+				width: 300,
+				height: 65,
+				rotationDeg: 5,
+			},
+		]
+	},
+	{
+		name: "Upper Dog Leg",
+		boxes: [
+			{
+				left: 1740,
+				top: 2695,
+				width: 125,
+				height: 110,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Red Dog Glades",
+		boxes: [
+			{
+				left: 1809,
+				top: 3258,
+				width: 250,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Christmas Tree",
+		boxes: [
+			{
+				left: 2226,
+				top: 3097,
+				width: 250,
+				height: 60,
+				rotationDeg: -40,
+			}
+		]
+	},
+	{
+		name: "Lower Dog Leg",
+		boxes: [
+			{
+				left: 2159,
+				top: 3168,
+				width: 250,
+				height: 60,
+				rotationDeg: -38,
+			}
+		]
+	},
+	{
+		name: "Secret Garden",
+		boxes: [
+			{
+				left: 2490,
+				top: 3411,
+				width: 250,
+				height: 60,
+				rotationDeg: -38,
+			}
+		]
+	},
+	{
+		name: "Cushman's",
+		boxes: [
+			{
+				left: 2397,
+				top: 3080,
+				width: 250,
+				height: 60,
+				rotationDeg: -36,
+			}
+		]
+	},
+	{
+		name: "Paris Glades",
+		boxes: [
+			{
+				left: 2337,
+				top: 2900,
+				width: 150,
+				height: 125,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Heidi's",
+		boxes: [
+			{
+				left: 2098,
+				top: 2573,
+				width: 120,
+				height: 60,
+				rotationDeg: -25,
+			}
+		]
+	},
+	{
+		name: "Heidi's Glades",
+		boxes: [
+			{
+				left: 2120,
+				top: 2477,
+				width: 250,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Red Dog Ridge",
+		boxes: [
+			{
+				left: 2076,
+				top: 2395,
+				width: 250,
+				height: 60,
+				rotationDeg: 43,
+			}
+		]
+	},
+	{
+		name: "Tom's Tumble",
+		boxes: [
+			{
+				left: 2298,
+				top: 2307,
+				width: 120,
+				height: 120,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "East Face Gully",
+		boxes: [
+			{
+				left: 2476,
+				top: 2500,
+				width: 250,
+				height: 60,
+				rotationDeg: -85,
+			}
+		]
+	},
+	{
+		name: "Tamara's",
+		boxes: [
+			{
+				left: 2382,
+				top: 2150,
+				width: 150,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "East Face Cliffs",
+		boxes: [
+			{
+				left: 2443,
+				top: 2353,
+				width: 160,
+				height: 60,
+				rotationDeg: 0,
+			},
+			{
+				left: 2500,
+				top: 2410,
+				width: 103,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Lower Champs Elysees",
+		boxes: [
+			{
+				left: 2685,
+				top: 2900,
+				width: 350,
+				height: 60,
+				rotationDeg: -92,
+			}
+		]
+	},
+	{
+		name: "Waterfall Gully",
+		boxes: [
+			{
+				left: 2730,
+				top: 2488,
+				width: 250,
+				height: 60,
+				rotationDeg: -75,
+			}
+		]
+	},
+	{
+		name: "Red Dog Face",
+		boxes: [
+			{
+				left: 2809,
+				top: 3469,
+				width: 250,
+				height: 60,
+				rotationDeg: -35,
+			}
+		]
+	},
+	{
+		name: "Women's Downhill",
+		boxes: [
+			{
+				left: 2694,
+				top: 2050,
+				width: 150,
+				height: 110,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Red Trail",
+		boxes: [
+			{
+				left: 2806,
+				top: 2598,
+				width: 150,
+				height: 60,
+				rotationDeg: 53,
+			}
+		]
+	},
+	{
+		name: "Downhill Cliffs",
+		boxes: [
+			{
+				left: 2899,
+				top: 2167,
+				width: 150,
+				height: 100,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "McConkey's",
+		boxes: [
+			{
+				left: 2911,
+				top: 1830,
+				width: 200,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "G.S. Bowl",
+		boxes: [
+			{
+				left: 3084,
+				top: 2068,
+				width: 175,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "G.S. Cliffs",
+		boxes: [
+			{
+				left: 3107,
+				top: 2376,
+				width: 175,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "G.S. Cliffs",
+		boxes: [
+			{
+				left: 2914,
+				top: 2655,
+				width: 190,
+				height: 120,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Easy Street",
+		boxes: [
+			{
+				left: 2900,
+				top: 2959,
+				width: 190,
+				height: 60,
+				rotationDeg: -25,
+			}
+		]
+	},
+	{
+		name: "Arnie's",
+		boxes: [
+			{
+				left: 3233,
+				top: 2220,
+				width: 120,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Nose",
+		boxes: [
+			{
+				left: 3424,
+				top: 2310,
+				width: 90,
+				height: 60,
+				rotationDeg: 0,
+			}
+		]
+	},
+	{
+		name: "Chute 75",
+		boxes: [
+			{
+				left: 3496,
+				top: 2034,
+				width: 150,
+				height: 60,
+				rotationDeg: -35,
+			}
+		]
+	},
+	{
+		name: "Nose Chutes",
+		boxes: [
+			{
+				left: 3466,
+				top: 2408,
+				width: 150,
+				height: 100,
+				rotationDeg: 0,
+			}
+		]
+	},
 ]
 
 var c = document.getElementById("map");
@@ -21,7 +470,9 @@ img.onload = function() {
 	drawImage()
 	initPanzoom()
 
-	block(runs[0])
+	for (let i=0; i < runs.length; i++) {
+		block(runs[i])
+	}
 }
 
 function drawImage() {
@@ -55,7 +506,14 @@ function calcMinScale() {
 }
 
 function block(run) {
-	ctx.beginPath()
-	ctx.fillRect(494*scale, 3294*scale, 57*scale, 145*scale)
-	ctx.stroke()
+	for (let i=0; i<run.boxes.length; i++) {
+		box = run.boxes[i]
+		ctx.save()
+		ctx.beginPath()
+		ctx.translate(box.left*scale, box.top*scale)
+		ctx.rotate(-box.rotationDeg * Math.PI / 180)
+		ctx.fillRect(0, 0, box.width*scale, box.height*scale)
+		ctx.stroke()
+		ctx.restore()
+	}
 }
