fe7f031a9becf291bfcc8ab34d7c78dffa880e09
Ben Holmes <7benholmes@gmail.com>
2023-11-15 16:57:40 -0800

    randomized run blocking with text input

--- main.js
+++ main.js
@@ -1,37 +1,61 @@
 var c = document.getElementById("map");
 var ctx = c.getContext("2d");
 var maxMobileCanvasSize = 16777216
-var scale
 
-// Load image
-var img = new Image()
-img.src = "assets/public-map.png"
+var guessInput = document.getElementById("guess")
+var panzoom_init_done = false
 
-img.onload = function() {
-	naturalPixels = img.naturalWidth * img.naturalHeight;
-	scale = maxMobileCanvasSize / naturalPixels
+blockRandomRun()
 
-	drawImage()
-	initPanzoom()
+guessInput.addEventListener('keydown', (event) => {
+	if (event.key === 'Enter') {
+		blockRandomRun()
+	}
+});
 
-	fetch("./annotation/output/public-map.json")
-	.then(response => {
-		return response.json();
-	}).then(runs => {
-		for (let i=0; i < runs.length; i++) {
-			block(runs[i])
-		}
-	});	
+function blockRandomRun() {
+	// ctx.clearRect(0, 0, c.width, c.height);
+	// ctx.beginPath()
+	var img = new Image()
+
+	img.onload = function() {	
+		drawImage(img)
+		initPanzoom()
+
+		fetch("./annotation/output/public-map.json")
+		.then(response => {
+			return response.json();
+		}).then(runs => {
+			idx = randInt(runs.length)
+			block(img, runs[idx])
+		});
+	}	
+	
+	img.src = "assets/public-map.png"
 }
 
-function drawImage() {
+function drawImage(img) {
+	scale = getImageScale(img)
+	console.log(img.naturalWidth, img.naturalHeight, scale)
+
 	c.width = img.naturalWidth * scale
 	c.height = img.naturalHeight * scale
+
+	console.log(c.width, c.height)
+
 	ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, c.width, c.height)
 }
 
 function initPanzoom() {
+	if (panzoom_init_done) {
+		return
+	}
+
+	panzoom_init_done = true
+
 	minScale = calcMinScale()
+	
+	console.log(minScale)
 
 	const panzoom = Panzoom(c, {
 		maxScale: 5,
@@ -45,16 +69,25 @@ function initPanzoom() {
 	c.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
 }
 
+function getImageScale(img) {
+	naturalPixels = img.naturalWidth * img.naturalHeight;
+	scale = maxMobileCanvasSize / naturalPixels
+
+	return scale;
+}
+
 function calcMinScale() {
 	containerWidth = document.getElementById("map-container").clientWidth
 	containerHeight = document.getElementById("map-container").clientHeight
-	imgWidth = document.getElementById("map").naturalWidth
-	imgHeight = document.getElementById("map").naturalHeight
+	imgWidth = c.width
+	imgHeight = c.height
 
 	return Math.max(containerWidth / imgWidth, containerHeight / imgHeight)
 }
 
-function block(run) {
+function block(img, run) {
+	scale = getImageScale(img)
+
 	for (let i=0; i<run.boxes.length; i++) {
 		box = run.boxes[i]
 		ctx.save()
@@ -65,4 +98,8 @@ function block(run) {
 		ctx.stroke()
 		ctx.restore()
 	}
+}
+
+function randInt(max) {
+	return Math.floor(Math.random() * max);
 }
